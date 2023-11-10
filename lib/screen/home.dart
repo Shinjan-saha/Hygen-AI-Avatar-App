20e3b08c9eabaca7909ab70894480fc8ebb7ce6c
Shinjan-saha <shinjansaha02@gmail.com>
2023-11-10 11:24:08 +0530

    Added App

---
+++ lib/screen/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter_plus/webview_flutter_plus.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: WebViewPlus(
+          javascriptMode: JavascriptMode.unrestricted,
+          initialUrl: 'https://www.microsoft-aurora.tech/',
+        ),
+      ),
+    );
+  }
+}
